1e6457bd8ca35a625f885465e60af9844da4aa1c
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2025-05-17 10:31:52 -0300

    feat: Add alert configuration for temperature sensors

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/domain/model/SensorAlert.java
@@ -0,0 +1,24 @@
+package com.algaworks.algasensors.temperature.monitoring.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SensorAlert {
+
+    @EmbeddedId
+    @AttributeOverride(name = "value",
+            column = @Column(name = "id", columnDefinition = "bigint"))
+    private SensorId id;
+    private Double maxTemperature;
+    private Double minTemperature;
+}
